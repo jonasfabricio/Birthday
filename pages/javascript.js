4119aed648b9b95f57072d1b08b7825824e9faab
jonasfabricio <jonas05fabrsousa@gmail.com>
2025-04-29 20:00:48 -0300

    finish

---
+++ pages/javascript.js
@@ -0,0 +1,27 @@
+function acertou(){
+    window.location = "http://127.0.0.1:5500/pages/index.html"
+}
+
+function errou(){
+    window.location = "http://127.0.0.1:5500/pages/errou.html"
+}
+
+function voltar(){
+    window.location = "http://127.0.0.1:5500/pages/login2.html"
+}
+
+function casa(){
+    window.location = "http://127.0.0.1:5500/pages/casa.html"
+}
+
+function cinema(){
+    window.location = "http://127.0.0.1:5500/pages/cinema.html"
+}
+
+function olinda(){
+    window.location = "http://127.0.0.1:5500/pages/olinda.html"
+}
+
+function escola(){
+    window.location = "http://127.0.0.1:5500/pages/secreta.html"
+}
